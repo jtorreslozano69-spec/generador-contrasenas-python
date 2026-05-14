6eb4ed01dd1b68ff351425fbc575c62ed4c77075
jtorreslozano69-spec <jtorreslozano69@gmail.com>
2026-05-14 15:57:39 -0500

    agregar comentarios

--- generador-contraseña.py
+++ generador-contraseña.py
@@ -1,14 +1,17 @@
 import random
 from werkzeug.security import generate_password_hash
 
+#los caracteres que se van a usar para generar la contraseña
 minus="abcdefghijklmnopqrstuvwxyz"
 mayus =minus.upper()
 numeros="0123456789"
 simbolos="!#$%&/()=?¿¡*+@"
 
+#la estructura de la contraseña, se pueden modificar el orden los caracteres y la longitud
 base=minus+mayus+numeros+simbolos
 longitud=12
 
+#generamos 10 contraseñas (range) aleatorias y las mostramos junto con su versión oculta
 for i in range(10):
 
  muestra=random.sample(base,longitud)
